78fba524556215dc3f8eac5bd368cccf15478b4b
sukrusimsek <sukrusimsekll@gmail.com>
2024-02-06 23:07:07 +0300

    first commit

---
+++ VERO-Digital-Solutions-ios-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VERO-Digital-Solutions-ios-task
+//
+//  Created by Şükrü Şimşek on 6.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
